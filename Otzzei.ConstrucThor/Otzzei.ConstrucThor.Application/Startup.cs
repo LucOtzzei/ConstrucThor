95197efe2addf18854ec0aee07ea234b8058206e
lucsantos21 <luc.olssay@outlook.com>
2023-02-24 16:26:56 -0300

    create startup

---
+++ Otzzei.ConstrucThor/Otzzei.ConstrucThor.Application/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Otzzei.ConstrucThor.Application
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+
+            var connectionString = Configuration.GetConnectionString("MinhaParoquiaDB");
+            services.AddDbContext<ConstrucThorContext>(opt => opt.UseSqlServer(connectionString));
+
+            services.AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(
+                opt => opt.SignIn.RequireConfirmedEmail = true)
+                .AddEntityFrameworkStores<ConstrucThorContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IUserService, UserService>();
+
+
+            services.AddMvc(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+        }
+    }
+
+    public interface IStartup
+    {
+        IConfiguration Configuration { get; }
+        void Configure(IApplicationBuilder app, IWebHostEnvironment environment);
+        void ConfigureServices(IServiceCollection services);
+
+    }
+}
+
